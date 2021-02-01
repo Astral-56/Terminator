8d450354a82541212a5e197620235847e3faa6b5
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-02-01 19:22:38 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -284,7 +284,7 @@ def get_help(bot: Bot, update: Update):
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="👩‍✈️Help",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="🔖Contact Creator",url="https://t.me/rosebakthan")]]))
+                                                [InlineKeyboardButton(text="🔖 Source Cod",url="https://t.me/Pr_o_To")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):

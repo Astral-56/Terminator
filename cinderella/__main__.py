6f87b172fffd92d09ef18bc8e8f93d57ea9e8f06
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-02-01 20:37:34 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -60,7 +60,7 @@ def vercheck() -> str:
 
 
 SOURCE_STRING = """
-⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/Sur-vivor/CinderellaProBot)
+⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://t.me/pr_o_To)
 ⚡You Can Clone Me [Here](https://t.me/joinchat/WCAe3d8Ou71LTErh)
 """
 

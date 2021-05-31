9948b74c5fe576f643e51e594390818f96d4cc73
Astral-56 <84642202+Astral-56@users.noreply.github.com>
2021-06-01 00:17:13 +0530

    Update tts.py

--- cinderella/modules/tts.py
+++ cinderella/modules/tts.py
@@ -39,6 +39,6 @@ def tts(bot: Bot, update: Update, args):
 __help__ = """ Text to speech
 - /tts <your text>
 """
-__mod_name__ = "TEXT TO SPEECH"
+__mod_name__ = "TTS"
 
 dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))

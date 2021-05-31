02e97ce082b0ea73910afebe42860a42411b87ce
Astral-56 <84642202+Astral-56@users.noreply.github.com>
2021-06-01 00:09:54 +0530

    Update feedback.py

--- cinderella/modules/feedback.py
+++ cinderella/modules/feedback.py
@@ -37,7 +37,7 @@ __help__ = """
                can can see your feeds here.
 """
 
-__mod_name__ = "FEEDBACK"
+__mod_name__ = "Feedback"
 
 feed_handle = DisableAbleCommandHandler("feedback", feedback)
 

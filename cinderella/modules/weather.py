653c671cf219fa0a362d02aff80ae0daa3afa212
Astral-56 <84642202+Astral-56@users.noreply.github.com>
2021-06-01 00:19:45 +0530

    Update weather.py

--- cinderella/modules/weather.py
+++ cinderella/modules/weather.py
@@ -83,7 +83,7 @@ __help__ = """
  - /weather <city>: gets weather info in a particular place using openweathermap.org api
 """
 
-__mod_name__ = "WEATHER"
+__mod_name__ = "Weather"
 
 WEATHER_HANDLER = DisableAbleCommandHandler("weather", weather, pass_args=True)
 

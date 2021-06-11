1dc417c0ed6309e3eeda526a8adf4841781e9eb6
Astral-56 <84642202+Astral-56@users.noreply.github.com>
2021-06-11 22:31:05 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -42,9 +42,7 @@ HELP_STRINGS = """
 ✪ /settings: 
    ◔ in PM: will send you your settings for all supported modules.
    ◔ in a Group: will redirect you to pm, with all that chat's settings.
-{}
-And the following:
-"""format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
+"""
 
 
 

4cb88cd269ce1318197c054652814ca49c004a87
Astral-56 <84642202+Astral-56@users.noreply.github.com>
2021-06-11 23:16:56 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -175,8 +175,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="❗Help",callback_data="help_back"),InlineKeyboardButton(text="News🔋",url="https://t.me/Luminous_Robot")]]
-    keyboard += [[InlineKeyboardButton(text="👥 Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="Add Me🔌",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="❔Help",callback_data="help_back"),InlineKeyboardButton(text="News🔋",url="https://t.me/Luminous_Robot")]]
+    keyboard += [[InlineKeyboardButton(text="👥Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="Add Me🔌",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
